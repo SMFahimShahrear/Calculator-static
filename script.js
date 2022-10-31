5a6a7b21dc3dda1477a286b0d883bea3764cf030
SMFahimShahrear <s.m.shahrear.fahim@gamil.com>
2022-10-31 21:55:57 +0600

    debug fix

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 let screen_1 = document.querySelector(".screen-1");
 let screen_2 = document.querySelector(".screen-2");
 let memory = [];
-const op_position_plus = [];
-const op_position_minus = [];
-const op_position_mul = [];
-const op_position_div = [];
-let i = 0, a = 0, b = 0, c=0, d= 0;
+let i = 0, j =0;
 let disp = '';
 let init = '';
 
@@ -19,36 +15,15 @@ function btn_call(e){
         i++;
         init = '';
 
-        if(e == "/"){
-            op_position_div[a] = i-1;
-            a++;
-        }
-        
-        else if(e == "x"){
-            op_position_mul[b] = i-1;
-            b++;
-        } 
-        
-        else if(e == "-"){
-            op_position_minus[c] = i-1;
-            c++;
-        } 
-        
-        else if(e == "+"){
-            op_position_plus[d] = i-1;
-            d++;
-        }
     }
     else if(e == "="){
-        console.log("yo");
+        for(j=1; j<=i; j+=2){
+            console.log(memory[j]);
+        }
     }
     else if(isNaN(e) == false){
         memory[i] = init;
         console.log("dun dun")
     }
     console.log(memory);
-    console.log(op_position_div);
-    console.log(op_position_mul);
-    console.log(op_position_minus);
-    console.log(op_position_plus);
 }
